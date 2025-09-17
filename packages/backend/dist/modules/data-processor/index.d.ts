5d98880cfd724c9d3fd53c0cf63aa039945912ae
Jiho Song <jihojiho2003@gmail.com>
2025-09-17 21:12:07 +0900

    remove dist in vcs

--- packages/backend/dist/modules/data-processor/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './types/funding-rate';
-export * from './parsers/log-parser';
-//# sourceMappingURL=index.d.ts.map
--- packages/backend/dist/modules/data-processor/parsers/log-parser.d.ts
+++
@@ -1,7 +0,0 @@
-import { RawLogData, LogProcessingResult } from '../types/funding-rate';
-export declare class LogParser {
-    static parseLogFile(rawData: RawLogData, logFileName: string): LogProcessingResult;
-    private static processFundingData;
-    static extractTimestampFromFileName(fileName: string): Date | null;
-}
-//# sourceMappingURL=log-parser.d.ts.map

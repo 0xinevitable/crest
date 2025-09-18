8edc7c8200475d24e66b7a19301091c2f7ea17f3
Junho Yeo <i@junho.io>
2025-09-18 11:14:26 +0900

    [core] Fetch spot/perp index from API

--- packages/core/typescript/fetch-index.ts
+++ packages/core/typescript/fetch-index.ts
@@ -1,26 +1,59 @@
 class HyperliquidAPI {
-  async getPerpIndex(symbol: string) {
+  async _requestInfo<T extends object>(request: { type: string }): Promise<T> {
     const response = await fetch('https://api.hyperliquid.xyz/info', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ type: 'meta' }),
+      body: JSON.stringify(request),
     });
-    const universe = (await response.json()).universe as {
-      szDecimals: number;
-      name: string;
-      maxLeverage: number;
-      marginTableId: number;
-    }[];
+    return response.json();
+  }
+
+  async getSpotIndex(symbol: string) {
+    const { tokens, universe } = await this._requestInfo<{
+      tokens: { name: string; index: number }[];
+      universe: {
+        index: number;
+        tokens: [tokenIndex: number, quoteTokenIndex: number];
+      }[];
+    }>({ type: 'spotMeta' });
+
+    const token = tokens.find((token) => token.name === symbol);
+    if (token?.index === undefined) {
+      throw new Error(`Token ${symbol} not found`);
+    }
+    const spot = universe.find((asset) => asset.tokens[0] === token.index);
+    if (spot?.index === undefined) {
+      throw new Error(`Spot ${symbol} not found`);
+    }
+    return {
+      tokenIndex: token.index,
+      spotIndex: spot.index,
+      meta: { token, spot },
+    };
+  }
+
+  async getPerpIndex(symbol: string) {
+    const { universe } = await this._requestInfo<{
+      universe: {
+        szDecimals: number;
+        name: string;
+        maxLeverage: number;
+        marginTableId: number;
+      }[];
+    }>({ type: 'meta' });
+
     const perpIndex = universe.findIndex((asset) => asset.name === symbol);
-    return { perpIndex, metadata: universe[perpIndex] };
+    return { perpIndex, meta: universe[perpIndex] };
   }
 }
 
 const main = async () => {
   const hl = new HyperliquidAPI();
 
-  const meta = await hl.getPerpIndex('HYPE');
-  console.log(meta);
+  const spot = await hl.getSpotIndex('HYPE');
+  console.log(spot);
+  const perp = await hl.getPerpIndex('HYPE');
+  console.log(perp);
 };
 
 main();

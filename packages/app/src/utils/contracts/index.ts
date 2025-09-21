2474c32dfd5adf8ebcd84b6e1f8f1e46432c5762
Jiho Song <jihojiho2003@gmail.com>
2025-09-21 22:23:40 +0900

    feat: add index file to export contract addresses and ABIs

---
+++ packages/app/src/utils/contracts/index.ts
@@ -0,0 +1,8 @@
+export { Contracts } from './addresses';
+export {
+  CrestTellerAbi,
+  CrestAccountantAbi,
+  CrestVaultAbi,
+  CrestManagerAbi,
+} from './abis';
+export { CrestClient, type Position } from './client';

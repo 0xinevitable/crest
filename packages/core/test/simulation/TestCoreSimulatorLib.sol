29f3908fced0d4078ae7cb9b903add2b01de468d
Junho Yeo <i@junho.io>
2025-09-22 00:57:27 +0900

    Fork `CoreExecution` to add `forcePosition`

--- packages/core/test/simulation/TestCoreSimulatorLib.sol
+++ packages/core/test/simulation/TestCoreSimulatorLib.sol
@@ -1,21 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {Vm} from "forge-std/Vm.sol";
+import { Vm } from "forge-std/Vm.sol";
 
-import {HyperCore} from "@hyper-evm-lib/test/simulation/HyperCore.sol";
-import {CoreWriterSim} from "@hyper-evm-lib/test/simulation/CoreWriterSim.sol";
-import {PrecompileSim} from "@hyper-evm-lib/test/simulation/PrecompileSim.sol";
-import {BboPrecompileSim} from "./BboPrecompileSim.sol";
+import { TestHyperCore } from "./TestHyperCore.sol";
+import { CoreWriterSim } from "@hyper-evm-lib/test/simulation/CoreWriterSim.sol";
+import { PrecompileSim } from "@hyper-evm-lib/test/simulation/PrecompileSim.sol";
+import { BboPrecompileSim } from "./BboPrecompileSim.sol";
 
-import {HLConstants} from "@hyper-evm-lib/src/PrecompileLib.sol";
+import { HLConstants } from "@hyper-evm-lib/src/PrecompileLib.sol";
 
 Vm constant vm = Vm(address(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D));
-CoreWriterSim constant coreWriter = CoreWriterSim(0x3333333333333333333333333333333333333333);
+CoreWriterSim constant coreWriter = CoreWriterSim(
+    0x3333333333333333333333333333333333333333
+);
 
 contract HypeSystemContract {
     receive() external payable {
-        coreWriter.nativeTransferCallback{value: msg.value}(msg.sender, msg.sender, msg.value);
+        coreWriter.nativeTransferCallback{ value: msg.value }(
+            msg.sender,
+            msg.sender,
+            msg.value
+        );
     }
 }
 
@@ -26,15 +32,17 @@ contract HypeSystemContract {
 library TestCoreSimulatorLib {
     uint256 constant NUM_PRECOMPILES = 17;
 
-    HyperCore constant hyperCore = HyperCore(payable(0x9999999999999999999999999999999999999999));
+    TestHyperCore constant hyperCore =
+        TestHyperCore(payable(0x9999999999999999999999999999999999999999));
 
     // ERC20 Transfer event signature
-    bytes32 constant TRANSFER_EVENT_SIG = keccak256("Transfer(address,address,uint256)");
+    bytes32 constant TRANSFER_EVENT_SIG =
+        keccak256("Transfer(address,address,uint256)");
 
-    function init() internal returns (HyperCore) {
+    function init() internal returns (TestHyperCore) {
         vm.pauseGasMetering();
 
-        HyperCore coreImpl = new HyperCore();
+        TestHyperCore coreImpl = new TestHyperCore();
 
         vm.etch(address(hyperCore), address(coreImpl).code);
         hyperCore.setStakingYieldIndex(1e18);
@@ -42,7 +50,9 @@ library TestCoreSimulatorLib {
 
         // Initialize precompiles
         for (uint160 i = 0; i < NUM_PRECOMPILES; i++) {
-            address precompileAddress = address(uint160(0x0000000000000000000000000000000000000800) + i);
+            address precompileAddress = address(
+                uint160(0x0000000000000000000000000000000000000800) + i
+            );
 
             // Use BboPrecompileSim for the BBO precompile (0x80e)
             if (precompileAddress == HLConstants.BBO_PRECOMPILE_ADDRESS) {
@@ -54,7 +64,9 @@ library TestCoreSimulatorLib {
         }
 
         // System addresses
-        address hypeSystemAddress = address(0x2222222222222222222222222222222222222222);
+        address hypeSystemAddress = address(
+            0x2222222222222222222222222222222222222222
+        );
         vm.etch(hypeSystemAddress, type(HypeSystemContract).runtimeCode);
 
         // Start recording logs for token transfer tracking
@@ -87,7 +99,12 @@ library TestCoreSimulatorLib {
                     uint64 tokenIndex = getTokenIndexFromSystemAddress(to);
 
                     // Call tokenTransferCallback on HyperCoreWrite
-                    hyperCore.executeTokenTransfer(address(0), tokenIndex, from, amount);
+                    hyperCore.executeTokenTransfer(
+                        address(0),
+                        tokenIndex,
+                        from,
+                        amount
+                    );
                 }
             }
         }
@@ -124,7 +141,11 @@ library TestCoreSimulatorLib {
         hyperCore.forceAccountActivation(account);
     }
 
-    function forceSpotBalance(address account, uint64 token, uint64 _wei) internal {
+    function forceSpotBalance(
+        address account,
+        uint64 token,
+        uint64 _wei
+    ) internal {
         hyperCore.forceSpotBalance(account, token, _wei);
     }
 
@@ -136,11 +157,26 @@ library TestCoreSimulatorLib {
         hyperCore.forceStakingBalance(account, _wei);
     }
 
-    function forceDelegation(address account, address validator, uint64 amount, uint64 lockedUntilTimestamp) internal {
-        hyperCore.forceDelegation(account, validator, amount, lockedUntilTimestamp);
+    function forceDelegation(
+        address account,
+        address validator,
+        uint64 amount,
+        uint64 lockedUntilTimestamp
+    ) internal {
+        hyperCore.forceDelegation(
+            account,
+            validator,
+            amount,
+            lockedUntilTimestamp
+        );
     }
 
-    function forceVaultEquity(address account, address vault, uint64 usd, uint64 lockedUntilTimestamp) internal {
+    function forceVaultEquity(
+        address account,
+        address vault,
+        uint64 usd,
+        uint64 lockedUntilTimestamp
+    ) internal {
         hyperCore.forceVaultEquity(account, vault, usd, lockedUntilTimestamp);
     }
 
@@ -148,7 +184,11 @@ library TestCoreSimulatorLib {
         hyperCore.setMarkPx(perp, markPx);
     }
 
-    function setMarkPx(uint32 perp, uint64 priceDiffBps, bool isIncrease) internal {
+    function setMarkPx(
+        uint32 perp,
+        uint64 priceDiffBps,
+        bool isIncrease
+    ) internal {
         hyperCore.setMarkPx(perp, priceDiffBps, isIncrease);
     }
 
@@ -156,7 +196,11 @@ library TestCoreSimulatorLib {
         hyperCore.setSpotPx(spotMarketId, spotPx);
     }
 
-    function setSpotPx(uint32 spotMarketId, uint64 priceDiffBps, bool isIncrease) internal {
+    function setSpotPx(
+        uint32 spotMarketId,
+        uint64 priceDiffBps,
+        bool isIncrease
+    ) internal {
         hyperCore.setSpotPx(spotMarketId, priceDiffBps, isIncrease);
     }
 
@@ -167,7 +211,12 @@ library TestCoreSimulatorLib {
         hyperCore.setStakingYieldIndex(multiplier);
     }
 
-    function forcePosition(address account, uint16 perpIndex, int64 szi, uint64 entryNtl) internal {
+    function forcePosition(
+        address account,
+        uint16 perpIndex,
+        int64 szi,
+        uint64 entryNtl
+    ) internal {
         hyperCore.forcePosition(account, perpIndex, szi, entryNtl);
     }
 
@@ -192,28 +241,44 @@ library TestCoreSimulatorLib {
         return false;
     }
 
-    function getTokenIndexFromSystemAddress(address systemAddr) internal pure returns (uint64) {
+    function getTokenIndexFromSystemAddress(
+        address systemAddr
+    ) internal pure returns (uint64) {
         if (systemAddr == address(0x2222222222222222222222222222222222222222)) {
             return 150; // HYPE token index
         }
-        return uint64(uint160(systemAddr) - uint160(0x2000000000000000000000000000000000000000));
+        return
+            uint64(
+                uint160(systemAddr) -
+                    uint160(0x2000000000000000000000000000000000000000)
+            );
     }
 
     function tokenExists(uint64 token) internal view returns (bool) {
-        (bool success,) = HLConstants.TOKEN_INFO_PRECOMPILE_ADDRESS.staticcall(abi.encode(token));
+        (bool success, ) = HLConstants.TOKEN_INFO_PRECOMPILE_ADDRESS.staticcall(
+            abi.encode(token)
+        );
         return success;
     }
 
     // BBO configuration functions
     function setBboForSpot(uint32 spotIndex) internal {
-        BboPrecompileSim(payable(HLConstants.BBO_PRECOMPILE_ADDRESS)).setAsSpot(uint64(spotIndex));
+        BboPrecompileSim(payable(HLConstants.BBO_PRECOMPILE_ADDRESS)).setAsSpot(
+            uint64(spotIndex)
+        );
     }
 
     function setBboForPerp(uint32 perpIndex) internal {
-        BboPrecompileSim(payable(HLConstants.BBO_PRECOMPILE_ADDRESS)).setAsPerp(uint64(perpIndex));
+        BboPrecompileSim(payable(HLConstants.BBO_PRECOMPILE_ADDRESS)).setAsPerp(
+            uint64(perpIndex)
+        );
     }
 
     function setBbo(uint64 assetId, uint64 bid, uint64 ask) internal {
-        BboPrecompileSim(payable(HLConstants.BBO_PRECOMPILE_ADDRESS)).setBbo(assetId, bid, ask);
+        BboPrecompileSim(payable(HLConstants.BBO_PRECOMPILE_ADDRESS)).setBbo(
+            assetId,
+            bid,
+            ask
+        );
     }
 }

d1ebc097c19cd08e0d088db7ea3dd40ce310da30
Jiho Song <jihojiho2003@gmail.com>
2025-09-22 11:15:56 +0900

    fix: update button label to reflect correct explorer name

--- packages/app/src/utils/toast.tsx
+++ packages/app/src/utils/toast.tsx
@@ -29,7 +29,7 @@ export const toastTransaction = async (promise: Promise<TransactionReceipt>) =>
                   win?.focus();
                 }}
               >
-                View in Mitoscan
+                View in Hyperliquid Explorer
               </button>
             </div>
           );
